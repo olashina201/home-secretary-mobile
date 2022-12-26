f4b04b28f24c8174a6b8c18cb5dac09ba835f67c
olashina201 <olashina201@gmail.com>
2022-12-26 19:39:59 +0100

    register interface

--- screens/Register.js
+++ screens/Register.js
@@ -8,6 +8,7 @@ import {
   AppFormField as FormField,
   SubmitButton,
 } from "../components/forms";
+import AppText from "../components/AppText";
 
 const validationSchema = Yup.object().shape({
   name: Yup.string().required().label("Name"),
@@ -20,7 +21,7 @@ const Register = () => {
     <Screen style={styles.container}>
       <Image
         style={styles.logo}
-        source={require("../../assets/logo-red.png")}
+        source={require("../assets/logo.png")}
       />
       <Form
         initialValues={{ name: "", email: "", password: "" }}
@@ -71,6 +72,7 @@ const Register = () => {
         <SubmitButton title="Register" />
         <SubmitButton title="Sign up with Google" />
       </Form>
+      <AppText style={styles.text}>Already have an account? Signin</AppText>
     </Screen>
   );
 };
@@ -79,7 +81,7 @@ const styles = StyleSheet.create({
   container: {
     padding: 10,
     width: "100%",
-    marginTop: 50,
+    marginTop: 10,
   },
   logo: {
     width: 80,
@@ -88,6 +90,10 @@ const styles = StyleSheet.create({
     marginTop: 50,
     marginBottom: 20,
   },
+  text: {
+    marginTop: 20,
+    textAlign: "center",
+  }
 });
 
 export default Register;
