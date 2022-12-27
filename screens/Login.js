c44413a666abee1c636413d53938f75caf82bbd1
olashina201 <olashina201@gmail.com>
2022-12-27 22:28:59 +0100

    Login and welcome screen

---
+++ screens/Login.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import { StyleSheet, Image } from "react-native";
+import Screen from "../components/Screen";
+import authApi from "../api/auth";
+import * as Yup from "yup";
+import {
+  ErrorMessage,
+  AppForm,
+  AppFormField,
+  SubmitButton,
+} from "../components/forms";
+import useAuth from "../auth/useAuth";
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string().required().label("Email"),
+  password: Yup.string().required().min(4).label("Password"),
+});
+const Login = () => {
+  const { login } = useAuth();
+  const [loginFailed, setLoginFailed] = useState(false);
+
+  const handleSubmit = async ({ username, password }) => {
+    const result = await authApi.login(username, password);
+    if (!result.ok) return setLoginFailed(true);
+    setLoginFailed(false);
+    login(result.data.token);
+  };
+
+  return (
+    <Screen style={styles.container}>
+      <Image
+        style={styles.logo}
+        source={require("../../assets/logo-red.png")}
+      />
+      <AppForm
+        initialValues={{ username: "", password: "" }}
+        onSubmit={handleSubmit}
+        validationSchema={validationSchema}
+      >
+        <ErrorMessage
+          error="Invalid username or password"
+          visible={loginFailed}
+        />
+        <AppFormField
+          autoCapitalize="none"
+          autoCorrect={false}
+          icon="email"
+          name="username"
+          placeholder="Username"
+        />
+        <AppFormField
+          autoCapitalize="none"
+          autoCorrect={false}
+          icon="lock"
+          name="password"
+          placeholder="Password"
+          textContentType="passowrd"
+          // secureTextEntry
+        />
+        <SubmitButton title="Login" />
+      </AppForm>
+    </Screen>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  logo: {
+    width: 80,
+    height: 80,
+    alignSelf: "center",
+    marginTop: 50,
+    marginBottom: 20,
+  },
+});
+
+export default Login;
---
+++ screens/Welcome.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
+import AppButton from "../components/AppButton";
+import routes from "../navigation/routes";
+
+const Welcome = ({ navigation }) => {
+  return (
+    <ImageBackground
+      blurRadius={5}
+      style={styles.background}
+      source={require("../../assets/background.jpg")}
+    >
+      <View style={styles.logoContainer}>
+        <Image
+          style={styles.logo}
+          source={require("../../assets/logo-red.png")}
+        />
+        <Text style={styles.tagline}>Sell Anything Online</Text>
+      </View>
+      <View style={styles.buttonsConatiner}>
+        <AppButton title="Login" onPress={() => navigation.navigate(routes.LOGIN)} />
+        <AppButton title="Register" color ="secondary" onPress={() => navigation.navigate(routes.REGISTER)} />
+      </View>
+    </ImageBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  buttonsConatiner: {
+    padding: 20,
+    width: "100%",
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignItems: "center",
+  },
+  tagline: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    paddingVertical: 20,
+  },
+});
+
+export default Welcome;
