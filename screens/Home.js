7e53cebdceac2a20692efed090c481508ff62d6e
olashina201 <olashina201@gmail.com>
2023-01-07 08:04:29 +0100

    home header

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,22 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import Screen from "../components/Screen";
+import AppText from "../components/AppText";
+import AppButton from "../components/AppButton";
+import colors from "../config/colors";
 
 const Home = () => {
   return (
     <Screen style={styles.container}>
-      <Text>Home</Text>
+      <View style={styles.header}>
+        <AppText style={styles.title}>Schedule</AppText>
+        <MaterialCommunityIcons
+          name="plus-circle"
+          size={50}
+          color={colors.primary}
+        />
+      </View>
     </Screen>
   );
 };
@@ -14,6 +25,17 @@ export default Home;
 
 const styles = StyleSheet.create({
   container: {
-    padding: 10,
+    padding: 20,
   },
+  header: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 25,
+    fontWeight: "bold",
+    color: colors.primary,
+  }
 });
